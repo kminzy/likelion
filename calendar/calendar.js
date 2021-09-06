29ef3ff501fd7731754f78ef2a60de23e4e494c3
kminzy <kminzy0704@gmail.com>
2021-09-07 00:19:22 +0900

    calendar css

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -57,11 +57,11 @@ window.onload = function () {
             for (let j = 0; j < 7; j++) {
                 //현재 달의 일수가 몇일인지 반환, 출력 종료조건
                 if (i === 0 && j < firstDate.getDay() || dateSet > pageYear[firstDate.getMonth()]) {
-                    //해당 칸에 날짜가 없으면 div엘리먼트만 생성한다.
+                    //해당 칸에 날짜가 없으면 div 생성
                     let dateEl = document.createElement('div');
                     weeklyEl.appendChild(dateEl);
                 } else {
-                    //해당 칸에 날짜가 있으면 div엘리먼트 생성 후 해당 날짜 넣어주기
+                    //해당 칸에 날짜가 있으면 div 생성 후 해당 날짜 put
                     let dateEl = document.createElement('div');
                     dateEl.textContent = dateSet;
                     dateEl.setAttribute('class', dateSet);
@@ -74,7 +74,6 @@ window.onload = function () {
             weekly++;
             calendarBody.appendChild(weeklyEl);
         }
-        // 현재 내가 선택한 날짜가 있으면 이전 달, 다음 달로 넘어가도 화면에 보여주기 위해 써줌
         let clickedDate = document.getElementsByClassName(today.getDate());
         clickedDate[0].classList.add('active');
     }
@@ -90,7 +89,7 @@ window.onload = function () {
         const mainYear = document.querySelector('.main-year');
         const mainMonth = document.querySelector('.main-month');
         const mainDate = document.querySelector('.main-date');
-        //const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        
         mainYear.innerHTML = today.getFullYear() + "년";
         mainMonth.innerHTML = today.getMonth() + 1 + "월";
         mainDate.innerHTML = today.getDate() + "일";
@@ -109,7 +108,6 @@ window.onload = function () {
     }
 
     function currentDateget() {
-        // format()을 이용해서 현재 날짜를 보기좋게 출력해주기 위해 사용.
         currentDate = today.format();
     }
 
@@ -164,7 +162,7 @@ window.onload = function () {
 
 }
 
-Date.prototype.format = function () { // 현재 날짜 보기좋게 출력 / 사용방법: newDate().format() 으로 사용가능
+Date.prototype.format = function () {
     var yyyy = this.getFullYear();
     var month = (this.getMonth() + 1);
     var dd = this.getDate();
@@ -172,7 +170,7 @@ Date.prototype.format = function () { // 현재 날짜 보기좋게 출력 / 사
     return format;
 }
 
-Date.prototype.format2 = function () { // 현재 날짜 보기좋게 출력 / 사용방법: newDate().format() 으로 사용가능
+Date.prototype.format2 = function () {
     var yyyy = this.getFullYear();
     var month = (this.getMonth() + 1);
     var format = [yyyy, month].join('-');
